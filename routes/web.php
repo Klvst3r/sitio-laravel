d78c4c0e2e70b9ace45a94981ca9389455bf539c
Klvst3r <klvst3r@gmail.com>
2024-12-26 09:33:10 -0600

    Rutas con nombres antes de blade

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/* Route::get('/', function () {
     return view('welcome');
 });
+ */
+Route::view('/', 'welcome')->name('home');
+
+//Contacto
+Route::view('/contacto', 'contact')->name('contact');
+
+//Blog
+Route::view('/blog', 'blog')->name('blog');
+
+// About
+Route::view('/about', 'about')->name('about');
+
