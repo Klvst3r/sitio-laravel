713a6344d3c06781c540a4ae5e871b1e6ba65400
Klvst3r <klvst3r@gmail.com>
2024-12-26 23:04:55 -0600

    Segunda forma - Plantillas de blade con componentes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,18 @@
-@component('components.layout')
+{{-- @component('components.layout') --}}
+
+
+{{-- <x-layout title="Inicio" meta-description="Home meta Description">
+    <x-slot name="title">
+        Home Title
+    </x-slot>
+
+</x-layout> --}}
+
+<x-layouts.app title="Home" :meta-description="'Home meta Description'" :sum="2 + 2">
+
     <h1>Inicio</h1>
-@endcomponent
+
+
+</x-layouts.app>
+
+{{-- @endcomponent --}}
